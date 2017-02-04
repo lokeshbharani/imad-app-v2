876392e1d4fc6c0d9273cf6e4222f4deba34774c
lokeshbharani <lokeshbharanikumarjk@gmail.com>
2017-02-03 22:41:27 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,41 @@ var artone={
     heading: 'Article One',
     date: 'Feb 3, 2017',
     content:`
-    Hi    dkkdk`
+            <p>
+                This is the content for my first article.
+            </p>
+            <p>
+                Never Give up.
+            </p>`
 };
+
+var htmltemp=`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link href="/ui/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+           ${heading}
+        </h3>
+        <div>
+           ${date}
+        </div>
+        <div>
+            ${content}
+            </div>
+        </div>
+    </body>
+
+</html>`
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
